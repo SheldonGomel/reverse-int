492b503c958e911345606d6f2179b810e14349af
VITALI STARASTSENKA <existing9@mail.ru>
2023-01-13 15:39:53 +0300

    feat: add index script file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+      // Your solution here
+      let result = "";
+      n = Math.abs(n);
+      let valstr = n + "";
+      for (let i = 0; i < valstr.length; i++) { 
+          result = result + valstr[valstr.length-i-1];
+          }
+      
+      return ((isNaN(Number(result))) ? "It is not Number" : Number(result));
 }
